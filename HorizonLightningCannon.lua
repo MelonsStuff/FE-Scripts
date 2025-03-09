ef685bc636e6908b669ee53f455834049e094a6d
MelonsStuff <140215929+MelonsStuff@users.noreply.github.com>
2025-03-09 18:49:23 -0500

    Create HorizonLightningCannon.lua

---
+++ HorizonLightningCannon.lua
@@ -0,0 +1,38 @@
+
+--[[
+   Credits:
+   MelonScripter - Converted and aligned
+   JustSolidify - Creator
+   Emper - Reanimate
+   MyWorld - Optimization
+      
+   [PAID] Weapons: https://www.roblox.com/catalog/17386491617/Void-RPG
+   https://www.roblox.com/catalog/17386482772/Void-Alligator-Jaw
+
+   [FREE] Fedora Rig:
+   // https://www.roblox.com/catalog/3033910400/International-Fedora-Germany
+   // https://www.roblox.com/catalog/3409612660/International-Fedora-USA
+   // https://www.roblox.com/catalog/3398308134/International-Fedora-Canada
+   // https://www.roblox.com/catalog/3033908130/International-Fedora-France
+   // https://www.roblox.com/catalog/4819740796/Robox
+
+   [PAID] Grey Rig:
+   // https://www.roblox.com/catalog/80479688830908/Gray-Torso-Extension
+   // https://www.roblox.com/catalog/80401121980206/GREY-Right-Leg
+   // https://www.roblox.com/catalog/74603792617282/GREY-Left-Leg
+   // https://www.roblox.com/catalog/111809732325428/clover-left-arm
+   // https://www.roblox.com/catalog/94138150908378/green-clover
+  
+   [PAID] Classic Rig:
+   // https://www.roblox.com/catalog/12344545199/Extra-Left-hand-moving-Blocky-white
+   // https://www.roblox.com/catalog/12344591101/Extra-Right-hand-moving-Blocky-white
+   // https://www.roblox.com/catalog/13779879140/Black-Fluffy-Fur-Body
+   // https://www.roblox.com/catalog/11263254795/Dummy-Head-For-Headless
+   // https://www.roblox.com/catalog/11159410305/Rectangle-Head-For-Headless
+   
+   Socials:
+   // https://x.com/MelonScripter
+   // https://www.youtube.com/@MelonScripter
+]]
+
+loadstring(game:HttpGet("https://gist.githubusercontent.com/MelonsStuff/34653df65434cbbbc36b3ec51d03e7a3/raw/8bd9ef0ad4495baa06f435629b562a103eaf2bf8/HorizonLC"))()
