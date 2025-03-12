956fa309bdbd28fd526eb774efde9ae63d635fd5
MelonsStuff <140215929+MelonsStuff@users.noreply.github.com>
2025-03-12 01:23:37 -0500

    Create Minigun.lua

---
+++ Minigun.lua
@@ -0,0 +1,39 @@
+--[[
+   Credits:
+   MelonScripter - Converted and aligned
+   ??? - Creator
+   Emper - Reanimate
+   MyWorld - Optimization
+      
+   [PAID] Minigun: https://www.roblox.com/catalog/121693295536898/Pixel-Bit-Heavy-Duty-Machine-Gun
+
+   [FREE] Minigun: https://www.roblox.com/catalog/15254947445/24k-Gold-Plunger
+   https://www.roblox.com/catalog/5552252553/Kinetic-Staff
+
+   [FREE] Fedora Rig:
+   // https://www.roblox.com/catalog/3033910400/International-Fedora-Germany
+   // https://www.roblox.com/catalog/3409612660/International-Fedora-USA
+   // https://www.roblox.com/catalog/3398308134/International-Fedora-Canada
+   // https://www.roblox.com/catalog/3033908130/International-Fedora-France
+   // https://www.roblox.com/catalog/4819740796/Robox
+
+   [PAID] Grey Rig:
+   // https://www.roblox.com/catalog/80479688830908/Gray-Torso-Extension
+   // https://www.roblox.com/catalog/80401121980206/GREY-Right-Leg
+   // https://www.roblox.com/catalog/74603792617282/GREY-Left-Leg
+   // https://www.roblox.com/catalog/111809732325428/clover-left-arm
+   // https://www.roblox.com/catalog/94138150908378/green-clover
+  
+   [PAID] Classic Rig:
+   // https://www.roblox.com/catalog/12344545199/Extra-Left-hand-moving-Blocky-white
+   // https://www.roblox.com/catalog/12344591101/Extra-Right-hand-moving-Blocky-white
+   // https://www.roblox.com/catalog/13779879140/Black-Fluffy-Fur-Body
+   // https://www.roblox.com/catalog/11263254795/Dummy-Head-For-Headless
+   // https://www.roblox.com/catalog/11159410305/Rectangle-Head-For-Headless
+   
+   Socials:
+   // https://x.com/MelonScripter
+   // https://www.youtube.com/@MelonScripter
+]]
+
+-- soon
