3a49a1c9f3a910fcec3a04a1db44056f33c74b56
MelonsStuff <140215929+MelonsStuff@users.noreply.github.com>
2025-03-16 16:48:40 -0500

    Update Minigun.lua

--- Minigun.lua
+++ Minigun.lua
@@ -1,39 +1,63 @@
 --[[
-   Credits:
-   MelonScripter - Converted and aligned
-   ??? - Creator
-   Emper - Reanimate
-   MyWorld - Optimization
-      
-   [PAID] Minigun: https://www.roblox.com/catalog/121693295536898/Pixel-Bit-Heavy-Duty-Machine-Gun
+Credits:
+MelonScripter - Converted and aligned
+??? - Creator
+Emper - Reanimate
+MyWorld - Optimization
 
-   [FREE] Minigun: https://www.roblox.com/catalog/15254947445/24k-Gold-Plunger
-   https://www.roblox.com/catalog/5552252553/Kinetic-Staff
+[PAID] Minigun: https://www.roblox.com/catalog/121693295536898/Pixel-Bit-Heavy-Duty-Machine-Gun
 
-   [FREE] Fedora Rig:
-   // https://www.roblox.com/catalog/3033910400/International-Fedora-Germany
-   // https://www.roblox.com/catalog/3409612660/International-Fedora-USA
-   // https://www.roblox.com/catalog/3398308134/International-Fedora-Canada
-   // https://www.roblox.com/catalog/3033908130/International-Fedora-France
-   // https://www.roblox.com/catalog/4819740796/Robox
+[FREE] Minigun: https://www.roblox.com/catalog/15254947445/24k-Gold-Plunger
+https://www.roblox.com/catalog/5552252553/Kinetic-Staff
 
-   [PAID] Grey Rig:
-   // https://www.roblox.com/catalog/80479688830908/Gray-Torso-Extension
-   // https://www.roblox.com/catalog/80401121980206/GREY-Right-Leg
-   // https://www.roblox.com/catalog/74603792617282/GREY-Left-Leg
-   // https://www.roblox.com/catalog/111809732325428/clover-left-arm
-   // https://www.roblox.com/catalog/94138150908378/green-clover
-  
-   [PAID] Classic Rig:
-   // https://www.roblox.com/catalog/12344545199/Extra-Left-hand-moving-Blocky-white
-   // https://www.roblox.com/catalog/12344591101/Extra-Right-hand-moving-Blocky-white
-   // https://www.roblox.com/catalog/13779879140/Black-Fluffy-Fur-Body
-   // https://www.roblox.com/catalog/11263254795/Dummy-Head-For-Headless
-   // https://www.roblox.com/catalog/11159410305/Rectangle-Head-For-Headless
-   
-   Socials:
-   // https://x.com/MelonScripter
-   // https://www.youtube.com/@MelonScripter
+[FREE] Fedora Rig:
+// https://www.roblox.com/catalog/3033910400/International-Fedora-Germany
+// https://www.roblox.com/catalog/3409612660/International-Fedora-USA
+// https://www.roblox.com/catalog/3398308134/International-Fedora-Canada
+// https://www.roblox.com/catalog/3033908130/International-Fedora-France
+// https://www.roblox.com/catalog/4819740796/Robox
+
+[PAID] Soldier Rig:
+// https://www.roblox.com/catalog/139102413583361/Metal-Arm
+// https://www.roblox.com/catalog/121830551276315/Prosthetic
+// https://www.roblox.com/catalog/99726531005445/Black-Prosthetic-Metal-Arm-RIGHT
+// https://www.roblox.com/catalog/18989526470/Black-Prosthetic-Metal-Arm-LEFT
+// https://www.roblox.com/catalog/74295684215899/Emo-Black-Armored-Vest
+// https://www.roblox.com/catalog/17510626021/Black-Star-Praetorian-Helmet
+
+[PAID] Spartan Rig:
+// https://www.roblox.com/catalog/120903328751560/Prosthetic-Arm
+// https://www.roblox.com/catalog/72858864947626/Prosthetic
+// https://www.roblox.com/catalog/101118281111918/Metal-Arm
+// https://www.roblox.com/catalog/98663604283708/Prosthetic-Arm
+// https://www.roblox.com/catalog/17171230401/Black-Torso
+// https://www.roblox.com/catalog/14898866310/Red-Alt-ODIN-Thor-Armor
+// https://www.roblox.com/catalog/14898895385/Red-Alt-ODIN-Thor-Helmet
+
+[PAID] Grey Rig:
+// https://www.roblox.com/catalog/80479688830908/Gray-Torso-Extension
+// https://www.roblox.com/catalog/80401121980206/GREY-Right-Leg
+// https://www.roblox.com/catalog/74603792617282/GREY-Left-Leg
+// https://www.roblox.com/catalog/111809732325428/clover-left-arm
+// https://www.roblox.com/catalog/94138150908378/green-clover
+
+[PAID] Classic Rig:
+// https://www.roblox.com/catalog/12344545199/Extra-Left-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/12344591101/Extra-Right-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/13779879140/Black-Fluffy-Fur-Body
+// https://www.roblox.com/catalog/11263254795/Dummy-Head-For-Headless
+// https://www.roblox.com/catalog/11159410305/Rectangle-Head-For-Headless
+
+[PAID] Classic Rig:
+// https://www.roblox.com/catalog/12344545199/Extra-Left-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/12344591101/Extra-Right-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/13779879140/Black-Fluffy-Fur-Body
+// https://www.roblox.com/catalog/11263254795/Dummy-Head-For-Headless
+// https://www.roblox.com/catalog/11159410305/Rectangle-Head-For-Headless
+
+Socials:
+// https://x.com/MelonScripter
+// https://www.youtube.com/@MelonScripter
 ]]
 
--- soon
+loadstring(game:HttpGet("https://gist.githubusercontent.com/MelonsStuff/e7b408abcb813525d37e9b7a6bf301c9/raw/9840c1650d299d36846f521ea6d87b9faffdc058/Minigun.txt"))()
