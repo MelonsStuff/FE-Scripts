17c72117774637e515e775d532b170252072ad5e
MelonsStuff <140215929+MelonsStuff@users.noreply.github.com>
2025-04-12 08:50:46 -0500

    Create Vergil.lua

---
+++ Vergil.lua
@@ -0,0 +1,76 @@
+
+_G.Settings = {
+["PermanentDeath"] = true, -- use permadeath method (only on supported executors)
+["ClickFling"] = true, -- click to fling player (recommended off since this uses damagefling)
+["Refit"] = true, -- refit to get lost hats during permadeath
+["HatDrop"] = false, -- drops adds to add collission to the fake rig
+["BreakJointsDelay"] = 0.225, -- how fast the player respawns (loop respawn method)
+}
+ 
+ _G.Fling = {
+["Prediction"] = true, -- accurate fling (stable) 
+["Timeout"] = 0.245, -- how long a player will be targetted during fling
+["HighlightTargets"] = true, -- highlight players being flung 
+["UseATool"] = false, -- use a tool to fling
+["UseAHat"] = false, -- uses a hat to fling (requires hat drop)
+}
+
+loadstring(game:HttpGet("https://gist.githubusercontent.com/MelonsStuff/82ea8c3cc5d32fc623c74bf74d728258/raw/3006f9717cb77ae9fe74a3416b6388eeb98bfc16/Vergil.lua"))()
+
+--[[
+[PAID] Weapons:
+// https://www.roblox.com/catalog/16171905812/Big-Kindle-Katana-Silver
+// https://www.roblox.com/catalog/16294169158/Big-Kindle-Katana-Deep-Blue
+// https://www.roblox.com/catalog/15008433643/Slug-Rifle-Red
+// https://www.roblox.com/catalog/14873591028/Slug-Pistol-Red
+
+[FREE] Weapons:
+// https://www.roblox.com/catalog/7548993875/Slasher
+// https://www.roblox.com/catalog/7583352167/Vector-Arrow
+// https://www.roblox.com/catalog/9867481700/Le-Rouge-Cross-body-Bag-1-0
+// https://www.roblox.com/catalog/9867487176/Le-Rouge-Cross-body-Bag-3-0
+
+[FREE] Fedora Rig:
+// https://www.roblox.com/catalog/3033910400/International-Fedora-Germany
+// https://www.roblox.com/catalog/3409612660/International-Fedora-USA
+// https://www.roblox.com/catalog/3398308134/International-Fedora-Canada
+// https://www.roblox.com/catalog/3033908130/International-Fedora-France
+// https://www.roblox.com/catalog/4819740796/Robox
+
+[PAID] Snake Banisher Rig:
+// https://www.roblox.com/catalog/79169258356641/Snakes-Vest-Torso
+// https://www.roblox.com/catalog/102600355076384/Snakes-Reaching-Right-Arm
+// https://www.roblox.com/catalog/90808514455706/Snakes-Reaching-Left-Arm
+// https://www.roblox.com/catalog/120111604252410/Thin-Hammer-Head-Black
+// https://www.roblox.com/catalog/88886554182275/Literal-Hammer-Head-Black
+
+[PAID] Soldier Rig:
+// https://www.roblox.com/catalog/139102413583361/Metal-Arm
+// https://www.roblox.com/catalog/121830551276315/Prosthetic
+// https://www.roblox.com/catalog/99726531005445/Black-Prosthetic-Metal-Arm-RIGHT
+// https://www.roblox.com/catalog/18989526470/Black-Prosthetic-Metal-Arm-LEFT
+// https://www.roblox.com/catalog/74295684215899/Emo-Black-Armored-Vest
+ 
+[PAID] Grey Rig:
+// https://www.roblox.com/catalog/80479688830908/Gray-Torso-Extension
+// https://www.roblox.com/catalog/80401121980206/GREY-Right-Leg
+// https://www.roblox.com/catalog/74603792617282/GREY-Left-Leg
+// https://www.roblox.com/catalog/111809732325428/clover-left-arm
+// https://www.roblox.com/catalog/94138150908378/green-clover
+
+[PAID] Classic Rig:
+// https://www.roblox.com/catalog/12344545199/Extra-Left-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/12344591101/Extra-Right-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/13779879140/Black-Fluffy-Fur-Body
+// https://www.roblox.com/catalog/11263254795/Dummy-Head-For-Headless
+// https://www.roblox.com/catalog/11159410305/Rectangle-Head-For-Headless
+
+Socials:
+// https://x.com/MelonScripter
+// https://www.youtube.com/@MelonScripter
+
+Credits:
+// MelonScripter - Converted and aligned
+// ZerX - Creator
+// Emper - Reanimate
+]]
