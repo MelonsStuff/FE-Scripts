01bf191db56368b7a089bc33171b54436ff959bf
MelonsStuff <140215929+MelonsStuff@users.noreply.github.com>
2025-04-18 22:44:10 -0500

    Create DisbeliefHubV2.lua

---
+++ DisbeliefHubV2.lua
@@ -0,0 +1,49 @@
+--[[
+Credits:
+MelonScripter - created disbelief hub
+ZerX - movement anim scripting
+Emper - reanimate
+
+All hats located @ https://www.roblox.com/games/15541318313/Melons-Testing-Melons-Baseplate
+
+[FREE] Fedora Rig:
+// https://www.roblox.com/catalog/3033910400/International-Fedora-Germany
+// https://www.roblox.com/catalog/3409612660/International-Fedora-USA
+// https://www.roblox.com/catalog/3398308134/International-Fedora-Canada
+// https://www.roblox.com/catalog/3033908130/International-Fedora-France
+// https://www.roblox.com/catalog/4819740796/Robox
+
+[PAID] Red Vest Rig:
+// https://www.roblox.com/catalog/79169258356641/Snakes-Vest-Torso
+// https://www.roblox.com/catalog/102600355076384/Snakes-Reaching-Right-Arm
+// https://www.roblox.com/catalog/90808514455706/Snakes-Reaching-Left-Arm
+// https://www.roblox.com/catalog/120111604252410/Thin-Hammer-Head-Black
+// https://www.roblox.com/catalog/88886554182275/Literal-Hammer-Head-Black
+
+[PAID] Grey Rig:
+// https://www.roblox.com/catalog/80479688830908/Gray-Torso-Extension
+// https://www.roblox.com/catalog/80401121980206/GREY-Right-Leg
+// https://www.roblox.com/catalog/74603792617282/GREY-Left-Leg
+// https://www.roblox.com/catalog/111809732325428/clover-left-arm
+// https://www.roblox.com/catalog/94138150908378/green-clover
+
+[PAID] Classic Rig:
+// https://www.roblox.com/catalog/12344545199/Extra-Left-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/12344591101/Extra-Right-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/13779879140/Black-Fluffy-Fur-Body
+// https://www.roblox.com/catalog/11263254795/Dummy-Head-For-Headless
+// https://www.roblox.com/catalog/11159410305/Rectangle-Head-For-Headless
+
+[PAID] Soldier Rig:
+// https://www.roblox.com/catalog/139102413583361/Metal-Arm
+// https://www.roblox.com/catalog/121830551276315/Prosthetic
+// https://www.roblox.com/catalog/99726531005445/Black-Prosthetic-Metal-Arm-RIGHT
+// https://www.roblox.com/catalog/18989526470/Black-Prosthetic-Metal-Arm-LEFT
+// https://www.roblox.com/catalog/74295684215899/Emo-Black-Armored-Vest
+ 
+Socials:
+// https://x.com/MelonScripter
+// https://www.youtube.com/@MelonScripter
+]]
+
+loadstring(game:HttpGet("https://gist.githubusercontent.com/MelonsStuff/55576b681073c3fa7db869b4feb7f313/raw/e4579c86fea0304eaca55e086ffd428730676001/DisbeliefDancer.lua"))()
