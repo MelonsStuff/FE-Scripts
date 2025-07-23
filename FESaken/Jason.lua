7d7d5013a7fdde1d33bcdffbd4ca7a57d48e1744
MelonsStuff <140215929+MelonsStuff@users.noreply.github.com>
2025-07-22 20:00:48 -0500

    Create Jason.lua

---
+++ FESaken/Jason.lua
@@ -0,0 +1,51 @@
+--[[
+ ______  ______   _____         _                
+|  ____||  ____| / ____|       | |               
+| |__   | |__   | (___    __ _ | | __ ___  _ __  
+|  __|  |  __|   \___ \  / _` || |/ // _ \| '_ \ 
+| |     | |____  ____) || (_| ||   <|  __/| | | |
+|_|     |______||_____/  \__,_||_|\_\\___||_| |_|
+       https://discord.gg/dM39KNvNj6
+
+[CREDITS]:
+// Melon - Scripted everything
+// ZerX - Movement scripting (last part only, i edited it a bit)
+// Emper - Reanimation
+// Forsaken - Animations
+
+[FREE] Fedora Rig:
+// https://www.roblox.com/catalog/3033910400/International-Fedora-Germany
+// https://www.roblox.com/catalog/3409612660/International-Fedora-USA
+// https://www.roblox.com/catalog/3398308134/International-Fedora-Canada
+// https://www.roblox.com/catalog/3033908130/International-Fedora-France
+// https://www.roblox.com/catalog/4819740796/Robox
+
+[PAID] Classic Rig:
+// https://www.roblox.com/catalog/12344545199/Extra-Left-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/12344591101/Extra-Right-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/13779879140/Black-Fluffy-Fur-Body
+// https://www.roblox.com/catalog/11263254795/Dummy-Head-For-Headless
+// https://www.roblox.com/catalog/11159410305/Rectangle-Head-For-Headless
+
+[PAID] Soldier Rig:
+// https://www.roblox.com/catalog/139102413583361/Metal-Arm
+// https://www.roblox.com/catalog/121830551276315/Prosthetic
+// https://www.roblox.com/catalog/99726531005445/Black-Prosthetic-Metal-Arm-RIGHT
+// https://www.roblox.com/catalog/18989526470/Black-Prosthetic-Metal-Arm-LEFT
+// https://www.roblox.com/catalog/74295684215899/Emo-Black-Armored-Vest
+
+// [PAID] Hats:
+// https://www.roblox.com/catalog/138480343439270/Two-Time-buddy
+// https://www.roblox.com/catalog/106118632964784/Jason-Vorhees-Machete-Area-51-Right
+// https://www.roblox.com/catalog/90928878496116/Jason
+
+// [FREE] Hats:
+// https://www.roblox.com/catalog/12495746361/VR-46-Turtle-Companion
+// https://www.roblox.com/catalog/11702906576/Battle-Beam
+// https://www.roblox.com/catalog/7548993875/Slasher
+
+Find the others here:
+// https://www.roblox.com/games/15541318313/Melons-Baseplate
+]]
+
+loadstring(game:HttpGet("https://gist.githubusercontent.com/MelonsStuff/6203b323781cfb0a7ad35e4e9f60e026/raw/222815c2a4f6ffe38f8ae3965f6b3640c180ab4c/Jason.lua"))()
