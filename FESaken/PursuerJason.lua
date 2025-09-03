68cda780d9624a4adf3d5b3044020bf93b7c06ff
MelonsStuff <140215929+MelonsStuff@users.noreply.github.com>
2025-09-02 19:49:33 -0500

    Create PursuerJason.lua

---
+++ FESaken/PursuerJason.lua
@@ -0,0 +1,56 @@
+--[[
+ ______  ______   _____         _                
+|  ____||  ____| / ____|       | |               
+| |__   | |__   | (___    __ _ | | __ ___  _ __  
+|  __|  |  __|   \___ \  / _` || |/ // _ \| '_ \ 
+| |     | |____  ____) || (_| ||   <|  __/| | | |
+|_|     |______||_____/  \__,_||_|\_\\___||_| |_|
+       
+[CREDITS]:
+// Melon - Scripted everything
+// ZerX - Movement scripting (last part only, i edited it a bit)
+// Emper - Reanimation
+// Forsaken - Animations
+// @cybop_ and @tacocat3071 - Rig/Hat suggestions
+
+[FREE] Hair Rig:
+// https://www.roblox.com/catalog/104558082150911/Pursuer
+// https://www.roblox.com/catalog/62724852/Chestnut-Bun
+// https://www.roblox.com/catalog/301818464/Lins-Hair
+// https://www.roblox.com/catalog/451220849/Lavender-Updo
+// https://www.roblox.com/catalog/376526888/Straight-Blonde-Hair
+// https://www.roblox.com/catalog/4819740796/Robox
+
+[FREE] Fedora Rig:
+// https://www.roblox.com/catalog/3033910400/International-Fedora-Germany
+// https://www.roblox.com/catalog/3409612660/International-Fedora-USA
+// https://www.roblox.com/catalog/4381707497/International-Fedora-Turkey
+// https://www.roblox.com/catalog/4645404679/International-Fedora-Thailand
+// https://www.roblox.com/catalog/4391384843/International-Fedora-Russia
+// https://www.roblox.com/catalog/3662265036/International-Fedora-Indonesia
+// https://www.roblox.com/catalog/4819740796/Robox
+
+[PAID] Classic Rig:
+// https://www.roblox.com/catalog/12344545199/Extra-Left-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/12344591101/Extra-Right-hand-moving-Blocky-white
+// https://www.roblox.com/catalog/13779879140/Black-Fluffy-Fur-Body
+// https://www.roblox.com/catalog/11263254795/Dummy-Head-For-Headless
+// https://www.roblox.com/catalog/11159410305/Rectangle-Head-For-Headless
+
+[PAID] Soldier Rig:
+// https://www.roblox.com/catalog/139102413583361/Metal-Arm
+// https://www.roblox.com/catalog/121830551276315/Prosthetic
+// https://www.roblox.com/catalog/99726531005445/Black-Prosthetic-Metal-Arm-RIGHT
+// https://www.roblox.com/catalog/18989526470/Black-Prosthetic-Metal-Arm-LEFT
+// https://www.roblox.com/catalog/74295684215899/Emo-Black-Armored-Vest
+
+// [PAID] Hats:
+https://www.roblox.com/catalog/98099894849025/VHS-Glitched-Dummy-Animated
+https://www.roblox.com/catalog/94622218233849/Pursuer-Die-of-Death-Sword-Waist
+https://www.roblox.com/catalog/92726717965695/Pursuer-Die-of-Death-Sword-Backpack
+
+Find the others here:
+// https://www.roblox.com/games/15541318313/Melons-Baseplate
+]]
+
+loadstring(game:HttpGet("https://gist.githubusercontent.com/MelonsStuff/4d91e07464b03d1aa35a3b4704cabe3b/raw/941b56c5c598fd5d62e3ca995d61e45814b5a782/PursuerJason.lua"))()
